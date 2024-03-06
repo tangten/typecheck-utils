0c45e10d0de16e8f038cd8d08a2974f67f092d96
Tang Nguyen Van <nvtang.khtn@gmail.com>
2024-03-06 16:40:01 +0700

    add search

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const areArrays = require('are-arrays');
-const areObjects = require('are-objects');
+const {
+  linearSearch,
+  binarySearch,
+  jumpSearch,
+  interpolationSearch,
+  ternarySearch,
+  fibonacciSearch,
+} = require("../src/search");
+const { isArrayOfObjects, isNestedObject } = require("../src/typecheck");
 
-// Our library functions
-function isArrayOfObjects(data) {
-  if (!areArrays(data)) return false; // Ensure it's an array at all
-
-  // Check each element within the array
-  return data.every(item => areObjects(item)); 
-}
-
-function isNestedObject(data) {
-  if (!areObjects(data)) return false;
-
-  // Check values for nested objects
-  return Object.values(data).some(value => areObjects(value));
-}
 
 // Exporting our functions
 module.exports = {
   isArrayOfObjects,
-  isNestedObject
+  isNestedObject,
+  linearSearch,
+  binarySearch,
+  jumpSearch,
+  interpolationSearch,
+  ternarySearch,
+  fibonacciSearch,
 };
